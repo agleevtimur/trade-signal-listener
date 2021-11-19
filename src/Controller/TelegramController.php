3a33f0073e0cf98d7aa6c042c21023a7a872f3bf
Timur Agleev <fantomas2213@gmail.com>
2021-11-19 14:12:29 +0300

    hardcode app conf

--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -6,6 +6,7 @@ use App\Event\NewSignalEvent;
 use App\Service\SignalHandlerManager;
 use danog\MadelineProto\API;
 use danog\MadelineProto\Settings;
+use danog\MadelineProto\Settings\AppInfo;
 use danog\MadelineProto\Settings\Database\Memory;
 use GuzzleHttp\Client;
 use Psr\Log\LoggerInterface;
@@ -20,6 +21,10 @@ class TelegramController extends AbstractController
         SignalHandlerManager::setServices($logger, new Client(), $parameterBag);
 
         $settings = new Settings();
+        $appInfo = new AppInfo();
+        $appInfo->setApiId(8003470)->setApiHash('84f8375bbe0abbc53c6e4f9ac4064550');
+
+        $settings->setAppInfo($appInfo);
         $settings->setDb(new Memory());
 
         $madeline = new API('session.madeline');
