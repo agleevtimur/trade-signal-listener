76ff763121cc2cecdc219dbb28cb6c6fee58127b
Timur Agleev <fantomas2213@gmail.com>
2021-12-13 00:16:06 +0300

    telegram-client -> python

---
+++ php/src/Controller/SignalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\SignalHandlerManager;
+use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class SignalController extends AbstractController
+{
+    public function receive(Request $request, LoggerInterface $logger, ParameterBagInterface $parameterBag): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $handler = new SignalHandlerManager($logger, new Client(), $parameterBag);
+
+        $handler->handle($data['peer'], $data['message']);
+
+        return $this->json(['status' => 'success']);
+    }
+}
---
+++ php/src/Service/SignalHandlerManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Service\R2BC\R2BCSignalHandler;
+use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class SignalHandlerManager
+{
+    private static array $handlerDictionary = [
+        'r2bc' => R2BCSignalHandler::class,
+    ];
+
+    public function __construct(private LoggerInterface $logger, private Client $client, private ParameterBagInterface $parameterBag) {}
+
+    public function handle(string $channel, string $message, int $messageId = 0): void
+    {
+        $handlerClass = static::$handlerDictionary[$channel] ?? null;
+        if ($handlerClass === null) {
+            return;
+        }
+
+        $this->logger->info('new signal received');
+        (new $handlerClass($this->logger, $this->client, $this->parameterBag))->resolve($message, $messageId);
+    }
+}
