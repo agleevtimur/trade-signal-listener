02042883e243cf7f32ac3927b637adc0dbbb6a89
Timur Agleev <fantomas2213@gmail.com>
2021-12-13 22:19:28 +0300

    telegram client -> python

---
+++ python/app.py
@@ -0,0 +1,32 @@
+from logging import PercentStyle
+from telethon import events
+from telethon.sync import TelegramClient
+import os, requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_ID = os.getenv('TELEGRAM_API_ID')
+API_HASH = os.getenv('TELEGRAM_API_HASH')
+R2BC_CHANNEL_ID = os.getenv('R2BC_CHANNEL_ID')
+USER_ID = os.getenv('TIMUR_USER_ID')
+URL = os.getenv('SIGNAL_RECEIVER_URL')
+
+client = TelegramClient('session', API_ID, API_HASH, sequential_updates=True).start()
+
+@client.on(events.NewMessage)
+async def event_handler(event):
+    print('new update')
+    if (event.chat is None):
+        peer = str(event.sender_id)
+    else:
+        peer = str(event.chat.id)
+
+    if (peer == R2BC_CHANNEL_ID or peer == USER_ID):
+        response = requests.post(url = URL, json = {"peer": "r2bc", "message": event.raw_text})
+        if (response.status_code != 200):
+            print('bad request', response.reason)
+
+client.start()
+client.run_until_disconnected()
